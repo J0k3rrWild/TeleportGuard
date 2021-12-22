647d61805d1de782ca76a5f7c195d9efff5df9c7
J0k3rrWild <alanpol857@gmail.com>
2021-12-22 21:41:33 +0100

    Added delayed tasks for tpa command

---
+++ src/J0k3rrWild/TeleportGuard/Commands/Tasks/TpaDelayed.php
@@ -0,0 +1,44 @@
+<?php 
+
+namespace J0k3rrWild\TeleportGuard\Commands\Tasks; 
+
+use pocketmine\scheduler\Task;
+use pocketmine\level\particle\FloatingTextParticle; 
+use pocketmine\level\Level;
+use pocketmine\math\Vector3;
+use J0k3rrWild\TeleportGuard\Commands\Tpa;
+use pocketmine\utils\TextFormat as TF;
+use pocketmine\utils\Config;
+
+class TpaDelayed extends Task{
+
+
+
+    public function __construct(Tpa $plugin, $cfg, $target, $sender){ 
+       $this->plugin = $plugin; 
+       $this->target = $target;
+       $this->sender = $sender;
+  
+       
+    } 
+
+
+    public function onRun(int $tick){ 
+
+      $cfg = new Config($this->plugin->main->getDataFolder()."players/". strtolower($this->sender->getName()) . "/temp.yml", Config::YAML);
+      $cfg->set("TpaCooldown", false);
+      $cfg->save();
+        
+         $jcfg = $this->plugin->main->getDataFolder()."players/". strtolower($this->target->getName()) . "/temp.json";
+                
+         $json = file_get_contents($jcfg);
+         $deco = json_decode($json, true);
+         $new = array_diff($deco, array($this->sender->getName())); 
+         file_put_contents($jcfg, json_encode($new)); 
+      
+     
+      
+     }
+        
+
+}
